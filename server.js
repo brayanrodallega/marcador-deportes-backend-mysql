ef8d3819778f239d6cf5b4fd183606fcd4bd1b6c
brayanrodallega <thebrayan1a1@outlook.com>
2022-11-26 22:54:59 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+const routes = require('./routes')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.set('port', 9000)
+
+const dbOptions = {
+    host: 'localhost',
+    port: '3306',
+    user: '',
+    password: '',
+    database: 'library'
+}
+
+// middelwares----------------------------------------------------
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json())
+
+// routes --------------------------------------------------------
+app.get('/', (req, res) => {
+    res.send('Welcome to my APP 2022')
+})
+
+app.use('/api', routes)
+
+app.listen(app.get('port'), ()=>{
+    console.log(`server running to port ${app.get('port')}`)
+})
