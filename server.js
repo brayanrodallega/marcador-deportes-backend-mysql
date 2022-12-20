feb1a77818908d0af7849ad3c46217e93ecfe041
brayanrodallega <thebrayan1a1@outlook.com>
2022-12-19 23:13:29 -0500

    fixed mysql connections

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const myconn = require('express-myconnection')
 const routes = require('./routes')
 const cors = require('cors')
